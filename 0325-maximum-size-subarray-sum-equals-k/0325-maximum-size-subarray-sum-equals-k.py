76ea337bd86a1055f6a60a02251f48ade3307615
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-12 11:22:28 -0400

    Time: 1646 ms (16.89%), Space: 57.4 MB (48.35%) - LeetHub

---
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Explanation : Two sum algorithm + prefix algorithm 
+    def maxSubArrayLen(self, nums: List[int], k: int) -> int:   
+        sums = {}
+        current = 0
+        sol = 0
+        
+        for i in range(0,len(nums)):
+            current += nums[i]
+            
+            if current == k:
+                sol = i + 1
+            
+            if current - k in sums:
+                sol = max(sol, i - sums[current - k ])
+            
+            if current not in sums:
+                sums[current] = i
+        return sol
