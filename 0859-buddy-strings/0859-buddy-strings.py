1deb0574810fbf849428200ea5839027ab70d794
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-13 00:56:12 -0400

    Time: 74 ms (21.18%), Space: 14.3 MB (20.28%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,33 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        
+        misplaced = None
+        misplacedLoc = None
+        swapped = False
+        seen = set()
+        repeat = set()
+        
+        for i in range(len(s)):
+            if s[i] in seen:
+                repeat.add(s[i])
+            else:
+                seen.add(s[i])
+            
+            print(s[i], goal[i], swapped)
+            if s[i] != goal[i]:
+                if swapped:
+                    return False
+                elif misplaced == None:
+                    misplaced = s[i]
+                    misplacedLoc = i
+                elif misplaced != None:
+                    if misplaced == goal[i] and s[i] == goal[misplacedLoc]:
+                        swapped = True
+                    else:
+                        return False
+        if misplaced != None and not swapped:
+            return False
+        return len(repeat) != 0 or swapped
+        
