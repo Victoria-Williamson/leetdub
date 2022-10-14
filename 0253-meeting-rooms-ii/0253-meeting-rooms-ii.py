b714bc94b99f41681222e84edc543c1ded50aebe
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-14 14:06:41 -0400

    Time: 367 ms (5.21%), Space: 17.8 MB (20.05%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,28 @@
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        filledRooms = []
+        roomsNeeded = 0
+        
+        intervals.sort()
+        
+      
+        for interval in intervals:
+            
+            if len(filledRooms) > 0:
+                print(filledRooms)
+                end, _ = filledRooms[0]
+                while end <= interval[0]:
+                    heapq.heappop(filledRooms)
+                    if len(filledRooms) != 0:
+                        end, _ = filledRooms[0]
+                    else:
+                        break
+                
+            heapq.heappush(filledRooms,(interval[1], interval[0]))
+            roomsNeeded = max(len(filledRooms), roomsNeeded)
+            print(filledRooms)
+           
+        return roomsNeeded
+    
+        
