f65d29669518fc377966e7a20f4827d4f2f93a40
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-17 18:12:38 -0400

    Time: 264 ms (17.58%), Space: 15.5 MB (88.63%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findOrder(self, numCourses: int, prereqs: List[List[int]]) -> List[int]:
+        numPreqs = [0 for i in range(numCourses)]
+        visited = [False for i in range(numCourses)]
+        q = []
+        nextCourses = {}
+        
+        for prereq in prereqs:
+            numPreqs[prereq[0]] += 1
+            
+            if prereq[1] not in nextCourses:
+                nextCourses[prereq[1]] = set()
+            nextCourses[prereq[1]].add(prereq[0])
+
+                
+        
+        for course in range(numCourses):
+            if numPreqs[course] == 0:
+                q.append(course)
+        
+        completed = []
+        while q:
+            course = q.pop(0)
+            if not visited[course]:
+                if course in nextCourses:
+                    
+                    for nextCourse in nextCourses[course]:
+                        numPreqs[nextCourse] -= 1
+                        if numPreqs[nextCourse] == 0 and not visited[nextCourse]:
+                            q.append(nextCourse)
+                     
+                   
+                completed.append(course)
+                visited[course] = True
+ 
+        if len(completed) == numCourses:
+            return completed
+        else:
+            return []
+        
