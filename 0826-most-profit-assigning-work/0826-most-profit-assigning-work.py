b91d413bc53f63b9e98055359c3948b73681165f
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-19 00:39:41 -0400

    Time: 1132 ms (14.91%), Space: 16.7 MB (96.57%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -2,9 +2,11 @@ class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
         jobs = [[-1 * profit[i], difficulty[i]] for i in range(len(profit))]
         heapq.heapify(jobs)
+        
         for i in range(len(worker)):
             worker[i] *= -1
         heapq.heapify(worker)
+        
         profits = 0
         while jobs:
             profit, difficulty = heapq.heappop(jobs)
