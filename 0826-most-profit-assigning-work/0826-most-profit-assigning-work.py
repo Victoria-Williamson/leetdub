d0c46a346d9c8aa3379dae8586bf04bcb11f2ee2
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-19 00:41:22 -0400

    Time: 1061 ms (19.25%), Space: 16.9 MB (95.39%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -3,16 +3,14 @@ class Solution:
         jobs = [[-1 * profit[i], difficulty[i]] for i in range(len(profit))]
         heapq.heapify(jobs)
         
-        for i in range(len(worker)):
-            worker[i] *= -1
-        heapq.heapify(worker)
+        worker.sort(reverse=True)
         
         profits = 0
         while jobs and worker:
             profit, difficulty = heapq.heappop(jobs)
             
-            while worker and difficulty <= -1 * worker[0]:
-                w = heapq.heappop(worker)
+            while worker and difficulty <=  worker[0]:
+                w = worker.pop(0)
                 profits -= profit
         return profits
             
