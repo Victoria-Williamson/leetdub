9be6d5dcc7b19b60d2dbdca70ed7651886ad6c33
Vic <63255236+Victoria-Williamson@users.noreply.github.com>
2022-10-26 19:20:23 -0400

    Time: 1114 ms (9.39%), Space: 21.8 MB (12.35%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -17,21 +17,12 @@ class Solution:
                 for xMove, yMove in [(1,0), (0,1), (-1,0), (0,-1)]:
                     addAround(xMove + x, yMove + y, dist + 1)
            
-        def findGate(x,y):
-                if not isValidMove(x,y):
-                    return 
-                elif rooms[x][y] == -1 or rooms[x][y] == INF:
-                    for xMove, yMove in  [(1,0), (0,1)]:
-                        findGate(x + xMove, y + yMove)
-                elif rooms[x][y] >= 0:
-                    for xMove, yMove in [(1,0), (0,1), (-1,0), (0,-1)]:
-                        addAround(x + xMove, y + yMove, rooms[x][y] + 1)
-                
         
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
-                    findGate(i,j)
+                    for xMove, yMove in [(1,0), (0,1), (-1,0), (0,-1)]:
+                        addAround(i + xMove, j + yMove, rooms[i][j] + 1)
         
        
             
